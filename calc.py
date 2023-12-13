bb9f93aeaed984e0a63de4479e1bbd6498d3eefe
ernests1c <149561992+ernests1c@users.noreply.github.com>
2023-12-13 10:19:42 +0200

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,24 +1,39 @@
 
 # konvertē datu mērvienības
 def megabit_to_megabyte(megabit):
-   result = 0
+   result = megabit * 8
    return result
 
 # saskaiti degvielas patēriņu uz 100 km
 def fuel_consumption(litres, kilometers):
-   result = 0
+   hundred = kilometers / 100
+   result = litres / hundred
    return result
 
 # konvertē temperatūru
 def celsius_to_fahrenheit(celsius):
-    result = 0
+    result = celsius * (9/5) + 32
     return result
 
 # saliec visus skaitļus pirms dota skaitļa (izmanto for)
-def sigma(tail):
-    result = 0
+def sigma(tail, x):
+    result =   0
     return result
 
 # nokonvertē svaru uz tuvāko mērvienību - grams, kilograms, tonna (izmanto if)
 def weight(grams):
-    return str(grams) + "g"
+     if grams >= 1000000:
+          result = int(grams / 1000000)
+          return str(result) + "t"
+     if grams >= 100000:
+        result =  int(grams / 100000)
+        return str(result) + "c"
+     if grams >= 10000:
+        result =  int(grams / 1000) 
+        return str(result) + "kg"
+     if grams >= 100:
+        result = int(grams)
+        return  str(result) + "g"
+    
+    
+   
